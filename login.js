9ce477f6145f8ce67687f1133f9e592c76dd9511
mysupernft-hash <mysupernft@gmail.com>
2026-01-22 05:43:35 +0400

    Update login.js

--- login.js
+++ login.js
@@ -1,28 +1,26 @@
-import { getAuth, signInWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-
-/* 🔥 SAME FIREBASE CONFIG */
 const firebaseConfig = {
   apiKey: "AIzaSyB4SGtNZL0N4TIoJ1bGbkiAeRWJcQgrF-4",
   authDomain: "supernft-5b952.firebaseapp.com",
   projectId: "supernft-5b952",
+  storageBucket: "supernft-5b952.appspot.com",
+  messagingSenderId: "278097730700",
   appId: "1:278097730700:web:6ba9892334456fd8512fa9"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-window.login = function(){
+document.getElementById("loginBtn").addEventListener("click", () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-.then(() => {
-  window.location.href = "dashboard.html";
-})
-.catch((error) => {
-  alert(error.message);
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
 });
