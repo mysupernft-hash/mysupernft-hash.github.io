288a3f4bf32bd0a5627ea5a6a6b090bffcce0641
mysupernft-hash <mysupernft@gmail.com>
2026-01-23 13:26:06 +0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
