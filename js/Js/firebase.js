92ed502b39fa8f07496c873fc9c6577a6526f8ff
mysupernft-hash <mysupernft@gmail.com>
2026-01-24 00:28:54 +0400

    Update firebase.js

--- js/Js/firebase.js
+++ js/Js/firebase.js
@@ -1,21 +1,20 @@
-// js/firebase.js
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyB4SGtNZL0N4TIoJ1bGbkiAeRWJcQgrF-4",
   authDomain: "supernft-5b952.firebaseapp.com",
   projectId: "supernft-5b952",
   storageBucket: "supernft-5b952.appspot.com",
   messagingSenderId: "278097730700",
-  appId: "1:278097730700:web:014d5ab680859db4512fa9"
+  appId: "1:278097730700:web:014d5ab680859db4512fa9",
+  measurementId: "G-BME7FWT355"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);   // Firebase Auth
-const db = getFirestore(app); // Firestore
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 export { auth, db };
